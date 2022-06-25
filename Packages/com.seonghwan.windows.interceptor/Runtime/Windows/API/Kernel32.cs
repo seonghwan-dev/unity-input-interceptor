a559e2816463b251b8b20e3ed52c61a7102f1b8b
seonghwan-dev <seonghwan@pm.me>
2022-06-25 22:33:55 +0900

    feat: #1 UPM 구성, Windows API 구현, Interceptor 기능 구현

---
+++ Packages/com.seonghwan.windows.interceptor/Runtime/Windows/API/Kernel32.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Windows.API
+{
+    public static class Kernel32
+    {
+        private const string DLL = "Kernel32";
+
+        /// <summary>
+        /// https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-getcurrentthreadid
+        /// </summary>
+        /// <returns></returns>
+        [DllImport(DLL)]
+        public static extern uint GetCurrentThreadId();
+    }
+}
